d69a17262e8b3b9f5e377ae739540e7fa8d1b1f4
Antonio <antoniocoru99@gmail.com>
2024-07-22 13:42:01 +0200

    ff

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -2,18 +2,17 @@ import React, { useEffect } from 'react';
 
 const AdBanner = () => {
   useEffect(() => {
+    const atOptions = {
+      'key': '9888b0aedf6f669c7dfb18958c8e54d6',
+      'format': 'iframe',
+      'height': 90,
+      'width': 728,
+      'params': {}
+    };
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.innerHTML = `
-      atOptions = {
-        'key' : '9888b0aedf6f669c7dfb18958c8e54d6',
-        'format' : 'iframe',
-        'height' : 90,
-        'width' : 728,
-        'params' : {}
-      };
-      document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/9888b0aedf6f669c7dfb18958c8e54d6/invoke.js"></scr' + 'ipt>');
-    `;
+    script.src = '//www.topcreativeformat.com/9888b0aedf6f669c7dfb18958c8e54d6/invoke.js';
+    script.async = true;
     document.getElementById('ad-banner').appendChild(script);
   }, []);
 
