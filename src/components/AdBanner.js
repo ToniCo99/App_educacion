a8a290e0dad46c2a89147fc2d9c1016f2a4083cb
Antonio <antoniocoru99@gmail.com>
2024-07-22 14:34:15 +0200

    7f

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -2,44 +2,28 @@ import React, { useEffect } from 'react';
 
 const AdBanner = () => {
   useEffect(() => {
-    const atOptions = {
-      key: '1f3a364caeb5808a02afe76486f86dcc',
-      format: 'iframe',
-      height: 50,
-      width: 320,
-      params: {}
-    };
-
-    const scriptContent = `
-      atOptions = ${JSON.stringify(atOptions)};
-      document.write('<scr' + 'ipt type="text/javascript" src="https://anddescendedcocoa.com/${atOptions.key}/invoke.js"></scr' + 'ipt>');
+    const adScript = document.createElement('script');
+    adScript.type = 'text/javascript';
+    adScript.innerHTML = `
+      atOptions = {
+        'key' : '1f3a364caeb5808a02afe76486f86dcc',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+      };
+      document.write('<scr' + 'ipt type="text/javascript" src="https://anddescendedcocoa.com/1f3a364caeb5808a02afe76486f86dcc/invoke.js"></scr' + 'ipt>');
     `;
+    const adContainer = document.getElementById('ad-container');
+    adContainer.appendChild(adScript);
 
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.innerHTML = scriptContent;
-
-    script.onload = () => {
+    adScript.onload = () => {
       console.log('Ad script loaded successfully');
-      const adContainer = document.getElementById('ad-container');
-      if (adContainer) {
-        console.log('Ad container content:', adContainer.innerHTML);
-      }
     };
 
-    script.onerror = (error) => {
+    adScript.onerror = (error) => {
       console.error('Error loading ad script:', error);
     };
-
-    const adContainer = document.getElementById('ad-container');
-    if (adContainer) {
-      console.log('Appending ad script to ad-container');
-      adContainer.innerHTML = ''; // Clear any previous content
-      adContainer.appendChild(script);
-    } else {
-      console.error('Ad container not found');
-    }
   }, []);
 
   return (
